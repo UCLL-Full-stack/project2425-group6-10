647273708e64d0f964d9e420a7eb74bbcbb399bf
Fabio Vangeel <fabio.vangeel@student.ucll.be>
2024-11-02 13:18:57 +0100

    user model + tests

---
+++ back-end/model/user.ts
@@ -0,0 +1,65 @@
+import { Role } from '../types';
+
+export class User {
+    private id?: number;
+    private username: string;
+    private email: string;
+    private password: string;
+    private role: Role;
+
+    constructor(user: {
+        id?: number;
+        username: string;
+        email: string;
+        password: string;
+        role: Role;
+    }) {
+        this.validate(user);
+        this.id = user.id;
+        this.username = user.username;
+        this.email = user.email;
+        this.password = user.password;
+        this.role = user.role;
+    }
+
+    getId(): number | undefined {
+        return this.id;
+    }
+
+    getUsername(): string {
+        return this.username;
+    }
+
+    getEmail(): string {
+        return this.email;
+    }
+
+    getPassword(): string {
+        return this.password;
+    }
+
+    getRole(): Role {
+        return this.role;
+    }
+
+    validate(user: { id?: number; username: string; email: string; password: string; role: Role }) {
+        if (!user.username?.trim()) {
+            throw new Error('Username is required');
+        }
+        if (!user.email?.trim()) {
+            throw new Error('Email is required');
+        }
+        if (!user.email.includes('@')) {
+            throw new Error('Email must be a valid email format.');
+        }
+        if (!user.password?.trim()) {
+            throw new Error('Password is required');
+        }
+        if (user.password.length < 8) {
+            throw new Error('Password must be at least 8 characters long');
+        }
+        if (user.role !== 'admin' && user.role !== 'student' && user.role !== 'lecturer') {
+            throw new Error('Role must be either admin, lecturer or student');
+        }
+    }
+}
---
+++ back-end/test/model/user.test.ts
@@ -0,0 +1,159 @@
+import { User } from '../../model/user';
+
+test('given: user with valid values, when: creating a new user, then: user is created', () => {
+    const user = new User({
+        username: 'JohnDoe',
+        email: 'john.doe@gmail.com',
+        password: 'John1234',
+        role: 'student',
+    });
+
+    expect(user.getUsername()).toBe('JohnDoe');
+    expect(user.getEmail()).toBe('john.doe@gmail.com');
+    expect(user.getPassword()).toBe('John1234');
+    expect(user.getRole()).toBe('student');
+});
+
+test('given: user with empty username, when: creating a new user, then: an error is thrown', () => {
+    const user = () => {
+        new User({
+            username: '',
+            email: 'john.doe@gmail.com',
+            password: 'John1234',
+            role: 'student',
+        });
+    };
+
+    expect(user).toThrow('Username is required');
+});
+
+test('given: user with only spaces in username, when: creating a new user, then: an error is thrown', () => {
+    const user = () => {
+        new User({
+            username: '       ',
+            email: 'john.doe@gmail.com',
+            password: 'John1234',
+            role: 'student',
+        });
+    }
+
+    expect(user).toThrow('Username is required');
+});
+
+test('given: user with empty email, when: creating a new user, then: an error is thrown', () => {
+    const user = () => {
+        new User({
+            username: 'JohnDoe',
+            email: '',
+            password: 'John1234',
+            role: 'student',
+        });
+    };
+
+    expect(user).toThrow('Email is required');
+});
+
+test('given: user with only spaces in email, when: creating a new user, then: an error is thrown', () => {
+    const user = () => {
+        new User({
+            username: 'JohnDoe',
+            email: '       ',
+            password: 'John1234',
+            role: 'student',
+        });
+    };
+
+    expect(user).toThrow('Email is required');
+});
+
+test('given: user with invalid email, when: creating a new user, then: an error is thrown', () => {
+    const user = () => {
+        new User({
+            username: 'JohnDoe',
+            email: 'john.doe.gmail.com',
+            password: 'John1234',
+            role: 'student',
+        });
+    };
+
+    expect(user).toThrow('Email must be a valid email format.');
+});
+
+test('given: user with empty password, when: creating a new user, then: an error is thrown', () => {
+    const user = () => {
+        new User({
+            username: 'JohnDoe',
+            email: 'john.doe@gmail.com',
+            password: '',
+            role: 'student',
+        });
+    };
+
+    expect(user).toThrow('Password is required');
+});
+
+test('given: user with only spaces in password, when: creating a new user, then: an error is thrown', () => {
+    const user = () => {
+        new User({
+            username: 'JohnDoe',
+            email: 'john.doe@gmail.com',
+            password: '      ',
+            role: 'student',
+        });
+    };
+
+    expect(user).toThrow('Password is required');
+});
+
+test('given: user with password less than 8 characters, when: creating a new user, then: an error is thrown', () => {
+    const user = () => {
+        new User({
+            username: 'JohnDoe',
+            email: 'john.doe@gmail.com',
+            password: 'john123',
+            role: 'student',
+        });
+    };
+
+    expect(user).toThrow('Password must be at least 8 characters long');
+});
+
+test('given: user with empty role, when: creating a new user, then: an error is thrown', () => {
+    const user = () => {
+        new User({
+            username: 'JohnDoe',
+            email: 'john.doe@gmail.com',
+            password: 'john1234',
+            role: '' as any,
+        });
+    };
+
+    expect(user).toThrow('Role must be either admin, lecturer or student');
+});
+
+test('given: user with only spaces role, when: creating a new user, then: an error is thrown', () => {
+    const user = () => {
+        new User({
+            username: 'JohnDoe',
+            email: 'john.doe@gmail.com',
+            password: 'john1234',
+            role: '     ' as any,
+        });
+    };
+
+    expect(user).toThrow('Role must be either admin, lecturer or student');
+});
+
+
+test('given: user with invalid role, when: creating a new user, then: an error is thrown', () => {
+    const user = () => {
+        new User({
+            username: 'JohnDoe',
+            email: 'john.doe@gmail.com',
+            password: 'john1234',
+            role: 'guest' as any,
+        });
+    };
+
+    expect(user).toThrow('Role must be either admin, lecturer or student');
+});
